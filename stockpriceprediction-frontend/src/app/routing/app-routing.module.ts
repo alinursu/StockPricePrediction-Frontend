6253fc23335551752e5235a4cd8a2f420e3ab018
Stefan-Alin Ursu <alinstefan.ursu@endava.com>
2021-11-22 16:14:33 +0200

    404 Not Found page

--- stockpriceprediction-frontend/src/app/routing/app-routing.module.ts
+++ stockpriceprediction-frontend/src/app/routing/app-routing.module.ts
@@ -5,6 +5,7 @@ import {LoginComponent} from "../modules/login/login.component";
 import {MainPageComponent} from "../modules/main-page/main-page.component";
 import {RegisterComponent} from "../modules/register/register.component";
 import {AllStocksComponent} from "../modules/all-stocks/all-stocks/all-stocks.component";
+import {NotFoundComponent} from "../modules/not-found/not-found/not-found.component";
 
 const routes: Routes = [
   {
@@ -27,6 +28,11 @@ const routes: Routes = [
     path: '',
     component: MainPageComponent,
     pathMatch: 'full'
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    component: NotFoundComponent
   }
   // TODO: Add 404 page
   // TODO: Surpress user access if he is logged in or not
